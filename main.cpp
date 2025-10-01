fe60f147939b7544a64c668fa687f4b579ad7e1f
Барышева Виктория Вадимовна <admin@localhost.localdomain>
2025-10-01 20:43:17 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
  printf("Введите своё имя:\n");
+<<<<<<< HEAD
  str = gets(str);
- printf("Привет, %s", str);
+ puts("Привет");
+ puts(str)
+>>>>>>> f6ed832 (Исправлено приветствие)
  return 0;
 }
